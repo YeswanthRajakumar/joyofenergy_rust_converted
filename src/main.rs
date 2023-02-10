e4eeb8b58c5e3ba373bd5f2173abeccd8474b782
Yeswanth Rajakumar <52456124+YeswanthRajakumar@users.noreply.github.com>
2023-02-10 12:43:22 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,26 @@ use humidity_sensor::HumiditySensor;
 use proximity_sensor::ProximitySensor;
 use temperature_sensor::TemperatureSensor;
 
-mod proximity_sensor;
 mod humidity_sensor;
+mod proximity_sensor;
 mod temperature_sensor;
 
+trait Readable{
+    fn get_reading(&self);
+}
 
 fn main() {
     let t = TemperatureSensor {};
     let p = ProximitySensor {};
     let h = HumiditySensor {};
 
-    for i in 0..10 {
-        print!("-> {} ", i);
-        print!("Temperature: {}", t.GetCurrentReading());
-        print!(", Proximity: {}", p.GetProximityState());
-        println!(", Humidity: {}", h.GetCurrentReading());
-    }
+    t.display();
+    p.display();
+    h.display();
+
+    // for i in 0..10 {
+    //     print!("-> {} ", i);
+    //     print!(", Proximity: {}", p.get_proximity_state());
+    //     println!(", Humidity: {}", h.get_current_reading());
+    // }
 }
