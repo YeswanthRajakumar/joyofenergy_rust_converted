956996cee21b4f7a188d7431e39811b1f1b50b5c
Yeswanth Rajakumar <52456124+YeswanthRajakumar@users.noreply.github.com>
2023-02-10 12:44:04 +0530

    Update proximity_sensor.rs

--- src/proximity_sensor.rs
+++ src/proximity_sensor.rs
@@ -5,8 +5,14 @@ pub struct ProximitySensor {}
 const PROXIMITY_BIT_INDEX: i32 = 4;
 
 impl ProximitySensor {
-    pub fn GetProximityState(&self) -> i32 {
+    pub fn get_proximity_state(&self) -> i32 {
         let r = rand::thread_rng().gen_range(120..900);
         ((r & (1 << PROXIMITY_BIT_INDEX)) > 0) as i32
+    }
+
+        pub fn display(&self) {
+        for _ in 0..10 {
+            print!("Proximity: {}\n", self.get_proximity_state());
+        }
     }
 }
